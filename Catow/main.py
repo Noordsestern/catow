f46e21547bdb8f30bb68c93f511f9c8d869f8a91
noordsestern <markus.i.sverige@gmail.com>
2021-07-01 22:47:08 +0200

    call robot service when workload available

--- Catow/main.py
+++ Catow/main.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 import logging
+import json
 import requests
 
 # Webservice
@@ -32,24 +33,40 @@ def run_server():
 async def watch_topic():
     while True:
         try:
-            response = requests.get(f'{CatowConfig().cmd_args.camundaurl}/engine-rest/external-task/count?topicName=aTopic')
+            response = requests.get(f'{CatowConfig().cmd_args.camundaurl}/engine-rest/external-task/count?topicName={CatowConfig().cmd_args.topic}')
             response.raise_for_status()
-            print(response)
+            print(response.text)
+            response_body = json.loads(response.text)
+            count = response_body["count"]
+            if count > 0:
+                print("Start Robot!")
+                try:
+                    response = requests.get(CatowConfig().cmd_args.roboturl)
+                    response.raise_for_status()
+                except Exception as e:
+                    print(f'[WARN] Failed to trigger robot:\n{e}')
+            else:
+                print("Nothing to do here.")
         except Exception as e:
             print(f'Failed to contact Camunda:\n{e}')
 
-        await asyncio.sleep(1)
+        await asyncio.sleep(CatowConfig().cmd_args.interval)
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--camundaurl", default='http://localhost:8080', help="URL of Camunda platform")
-    #parser.add_argument('--version', action='version', version=f'Camunda Topic Watcher Webservice {get_version()}')
     parser.add_argument("-p", "--port", default=os.environ.get('CATOW_PORT', default=5003), type=int,
                         help="Port of Camunda Topic Watcher Webservice")
-    parser.add_argument("-i", "--intervall", default=3, type=int,
-                        help="Polling intervall in seconds")
+    parser.add_argument("-c", "--camundaurl", default='http://localhost:8080', help="URL of Camunda platform")
+    parser.add_argument("-t", "--topic", default="aTopic", type=str,
+                        help="Topic of service task to listen for on Camunda")
+    parser.add_argument("-r", "--roboturl", default='http://localhost:5004', type=str,
+                        help="Url to be called when workload is detected")
+    #parser.add_argument('--version', action='version', version=f'Camunda Topic Watcher Webservice {get_version()}')
+    parser.add_argument("-i", "--interval", default=3, type=int,
+                        help="Polling interval in seconds")
+
     args = parser.parse_args()
 
     CatowConfig().cmd_args = args
