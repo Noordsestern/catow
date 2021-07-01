e7dad2494015a6f0c6ed3aa359b89526afac551a
Noordsestern <markus.i.sverige@googlemail.com>
2021-07-01 15:44:23 +0200

    init

--- Catow/main.py
+++ Catow/main.py
@@ -1,9 +1,10 @@
 import os
 import asyncio
-
+import logging
 import requests
+
+# Webservice
 from fastapi import FastAPI
-from requests import HTTPError
 from uvicorn import Server
 from uvicorn.config import Config
 
@@ -14,7 +15,7 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-    asyncio.create_task(watch_topic())
+    asyncio.run_coroutine_threadsafe(watch_topic(), asyncio.get_event_loop())
 
 
 @app.get("/")
@@ -31,9 +32,9 @@ def run_server():
 async def watch_topic():
     while True:
         try:
-            future = asyncio.run_coroutine_threadsafe(requests.get(CatowConfig().cmd_args.camundaurl))
-            response = future.result()
+            response = requests.get(f'{CatowConfig().cmd_args.camundaurl}/engine-rest/external-task/count?topicName=aTopic')
             response.raise_for_status()
+            print(response)
         except Exception as e:
             print(f'Failed to contact Camunda:\n{e}')
 
@@ -45,7 +46,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--camundaurl", default='http://localhost:8080', help="URL of Camunda platform")
     #parser.add_argument('--version', action='version', version=f'Camunda Topic Watcher Webservice {get_version()}')
-    parser.add_argument("-p", "--port", default=os.environ.get('CATOW_PORT', default=5003), type=int, help="Port of Camunda Topic Watcher Webservice")
+    parser.add_argument("-p", "--port", default=os.environ.get('CATOW_PORT', default=5003), type=int,
+                        help="Port of Camunda Topic Watcher Webservice")
+    parser.add_argument("-i", "--intervall", default=3, type=int,
+                        help="Polling intervall in seconds")
     args = parser.parse_args()
 
     CatowConfig().cmd_args = args
